8100021006c06750b290f475bf34dd8ea4918b52
anna0312 <annabremner@gmail.com>
2018-08-28 10:17:42 -0400

    No update, just wanted to switch branches and had deleted some old commented out code.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,24 +4,6 @@ const participants = require('./graphs/participants')
 
 $(() => {
   // your JS code goes here
-  participants.addHandlers()
   participants.loadParticipantsGraph()
-//  donations.addHandlers()
-//  donations.loadDonationsGraph()
-
-// set some default variables on pageLoadEvents
-  // const graphSubject = $("input[name='graphSubject']:checked").val()
-  // if (graphSubject === 'Donations') {
-  //   if ($('#projectedTotal').val() === '0') {
-  //     $('#projectedTotal').val(2207000)
-  //   }
-  // } else {
-  //   if ($('#projectedTotal').val() === '0') {
-  //     $('#projectedTotal').val(14026)
-  //   }
-  // }
-  // if ($('#dateStart').val() === '') {
-  //   $('#dateStart').val(moment().format('M/D/YYYY'))
-  // }
-
+  participants.addHandlers()
 })
